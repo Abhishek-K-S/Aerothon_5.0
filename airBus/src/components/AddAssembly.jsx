111f0fcdd4f2a70ce85109c1023a73f7a2d7164c
dhananaya-b <dhananjayab302@gmail.com>
2023-05-13 13:04:31 +0530

    added add details

--- airBus/src/components/AddAssembly.jsx
+++ airBus/src/components/AddAssembly.jsx
@@ -1,9 +1,122 @@
-import React from 'react'
+import React from "react";
+
 
 const AddAssembly = () => {
   return (
-    <div>AddAssembly</div>
-  )
-}
+    //assembly
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <form
+        style={{
+          width: "70%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          // className="inpContainer"
+          style={{
+            // display: "flex",
+            // flexDirection: "column",
+            marginTop: "3%",
+          }}
+        >
+          <div
+            // className="inputHold"
+            style={{ width: "100%", display: "flex" }}
+          >
+            <label htmlFor="sub_assembly">Sub Assembly : </label>
+          </div>
+          <select
+            name="sub_assembly"
+            id="sub_assembly"
+            placeholder="Roles"
+            style={{ width: "450px" }}
+          >
+            <option value="sub_1">sub_1</option>
+          </select>
+        </div>
+        <div
+          style={{
+            // display: "flex",
+            // flexDirection: "column",
+            marginTop: "3%",
+          }}
+        >
+          <div
+            // className="inputHold"
+            style={{ width: "50%", display: "flex" }}
+          >
+            <label htmlFor="item">Process Name : </label>
+          </div>
 
-export default AddAssembly
+          <input
+            placeholder={"process_name"}
+            style={{ marginBottom: "10px", width: "450px" }}
+            id={"process_name"}
+          />
+        </div>
+        <div
+          style={{
+            // display: "flex",
+            // flexDirection: "column",
+            marginTop: "3%",
+          }}
+        >
+          <div
+            // className="inputHold"
+            style={{ width: "50%", display: "flex" }}
+          >
+            <label htmlFor="in_date">In Date : </label>
+          </div>
+
+          <input
+            placeholder={"In Date"}
+            style={{ marginBottom: "10px", width: "450px" }}
+            id={"in_date"}
+          />
+        </div>
+        <div
+          style={{
+            // display: "flex",
+            // flexDirection: "column",
+            marginTop: "3%",
+          }}
+        >
+          <div
+            // className="inputHold"
+            style={{ width: "50%", display: "flex" }}
+          >
+            <label htmlFor="out_date">Out date : </label>
+          </div>
+
+          <input
+            placeholder={"Out date"}
+            style={{ marginBottom: "10px", width: "450px" }}
+            id={"out_date"}
+          />
+        </div>
+        <div
+          style={{
+            // display: "flex",
+            // flexDirection: "column",
+            marginTop: "3%",
+          }}
+        >
+          <button type="submit" className="btn" style={{ width: "450px" }}>
+            Add Data
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddAssembly;
--- airBus/src/components/AddFrabircation.jsx
+++ airBus/src/components/AddFrabircation.jsx
@@ -1,9 +1,140 @@
-import React from 'react'
+import React from "react";
+
 
 const AddFrabircation = () => {
   return (
-    <div>AddFrabircation</div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <form
+        style={{
+          border: "2px solid red",
+          width: "70%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          // className="inpContainer"
+          style={{
+            // display: "flex",
+            // flexDirection: "column",
+            marginTop: "3%",
+          }}
+        >
+          <div
+            // className="inputHold"
+            style={{ width: "50%", display: "flex" }}
+          >
+            <label htmlFor="item">Item : </label>
+          </div>
+          <input
+            placeholder={"Item"}
+            style={{ marginBottom: "10px", width: "450px" }}
+            id={"item"}
+          />
+        </div>
+        <div
+          style={{
+            // display: "flex",
+            // flexDirection: "column",
+            marginTop: "3%",
+          }}
+        >
+          <div
+            // className="inputHold"
+            style={{ width: "50%", display: "flex" }}
+          >
+            <label htmlFor="item">Raw Material : </label>
+          </div>
+
+          <input
+            placeholder={"Raw Material"}
+            style={{ marginBottom: "10px", width: "450px" }}
+            id={"raw_material"}
+          />
+        </div>
+        <div
+          style={{
+            // display: "flex",
+            // flexDirection: "column",
+            marginTop: "3%",
+          }}
+        >
+          <div
+            // className="inputHold"
+            style={{ width: "50%", display: "flex" }}
+          >
+            <label htmlFor="item">Quantity : </label>
+          </div>
+
+          <input
+            placeholder={"Quantity"}
+            style={{ marginBottom: "10px", width: "450px" }}
+            id={"quantity"}
+          />
+        </div>
+        <div
+          style={{
+            // display: "flex",
+            // flexDirection: "column",
+            marginTop: "3%",
+          }}
+        >
+          <div
+            // className="inputHold"
+            style={{ width: "50%", display: "flex" }}
+          >
+            <label htmlFor="indate">In date : </label>
+          </div>
+
+          <input
+            placeholder={"In date"}
+            style={{ marginBottom: "10px", width: "450px" }}
+            id={"indate"}
+          />
+        </div>
+        <div
+          style={{
+            // display: "flex",
+            // flexDirection: "column",
+            marginTop: "3%",
+          }}
+        >
+          <div
+            // className="inputHold"
+            style={{ width: "50%", display: "flex" }}
+          >
+            <label htmlFor="outdate">Out date : </label>
+          </div>
+
+          <input
+            placeholder={"Out date : "}
+            style={{ marginBottom: "10px", width: "450px" }}
+            id={"outdate"}
+          />
+        </div>
+
+        <div
+          style={{
+            // display: "flex",
+            // flexDirection: "column",
+            marginTop: "3%",
+          }}
+        >
+          <button type="submit" className="btn" style={{ width: "450px" }}>
+            Add Data
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default AddFrabircation;
